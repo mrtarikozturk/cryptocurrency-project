32becc46388ca81049ff2e44e1db9e5f335516ed
Mr. Tarik OZTURK <mr.tarik.ozturk@gmail.com>
2021-11-01 19:00:05 +0100

    Added some config and prettier adjusment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from "react"
+import React from 'react';
 
-const App :React.FC= ()=> {
-  return (
-    <div >
-    Cryptocurrency Project
-    </div>
-  );
-}
+const App: React.FC = () => {
+  return <div>Cryptocurrency Project</div>;
+};
 
 export default App;
